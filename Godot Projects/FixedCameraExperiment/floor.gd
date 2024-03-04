cd737b97b627e2331cd6212f46951a08408d99f8
Noodledoodlemacaroodle <160121077+Noodledoodlemacaroodle@users.noreply.github.com>
2024-03-04 22:22:11 +0100

    recommit

---
+++ Godot Projects/FixedCameraExperiment/floor.gd
@@ -0,0 +1,11 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass #hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
